c5e46062e4b2b9983ef281ee057912cf7a3d4944
nalabjp <nalabjp@gmail.com>
2019-08-31 20:00:28 +0900

    Add LeftClick, RightClick

--- keyboards/ergodox_ez/keymaps/nalabjp/keymap.c
+++ keyboards/ergodox_ez/keymaps/nalabjp/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   ` ~  |  1 ! |  2 @ |  3 # |  4 $ |  5 % |      |           |      |  6 ^ |  7 & |  8 * |  9 ( |  0 ) |  - _   |
+ * |   ` ~  |  1 ! |  2 @ |  3 # |  4 $ |  5 % | Lclk |           | Rclk |  6 ^ |  7 & |  8 * |  9 ( |  0 ) |  - _   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [   |           |  _   |   Y  |   U  |   I  |   O  |   P  |  \ |   |
  * |--------+------+------+------+------+------|  {   |           |  -   |------+------+------+------+------+--------|
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-         KC_GRV,     KC_1,     KC_2,    KC_3,            KC_4,    KC_5, KC_TRNS,
+         KC_GRV,     KC_1,     KC_2,    KC_3,            KC_4,    KC_5, KC_BTN1,
          KC_TAB,     KC_Q,     KC_W,    KC_E,            KC_R,    KC_T, KC_LBRC,
         KC_LCTL,     KC_A,     KC_S,    KC_D,            KC_F,    KC_G,
         KC_LSFT,     KC_Z,     KC_X,    KC_C,            KC_V,    KC_B, KC_RBRC,
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                         KC_PGUP,
                                                 SFT_T(KC_ESC), KC_BSPC, KC_PGDN,
         // right hand
-        KC_TRNS, KC_6,   KC_7,            KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_BTN2, KC_6,   KC_7,            KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_MINS, KC_Y,   KC_U,            KC_I,    KC_O,    KC_P,    KC_BSLS,
                  KC_H,   KC_J,            KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_EQL,  KC_N,   KC_M,            KC_COMM, KC_DOT,  KC_SLSH, LALT(KC_SPC),
